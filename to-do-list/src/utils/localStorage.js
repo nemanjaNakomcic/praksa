2fb0ffd545d44dae608eec764153a805189e0b40
nemanjaNakomcic <93490129+nemanjaNakomcic@users.noreply.github.com>
2025-05-06 12:28:55 +0200

    Revert "Todo task list React app"

--- to-do-list/src/utils/localStorage.js
+++
@@ -1,8 +0,0 @@
-export const loadTasks = () => {
-    const data = localStorage.getItem('tasks');
-    return data ? JSON.parse(data) : [];
-};
-
-export const saveTasks = (tasks) => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-}
